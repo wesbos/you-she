4166a39d0b89822cbef84d7f30f95528863dc635
Wes Bos <wesbos@gmail.com>
2021-09-07 16:54:52 -0400

    add photo

--- src/lib/Person.svelte
+++ src/lib/Person.svelte
@@ -1,9 +1,25 @@
 <script>
 	import PrismicDom from 'prismic-dom';
 	export let details;
+	console.log(details);
 </script>
 
 <div class="person">
 	{@html PrismicDom.RichText.asHtml(details.name)}
+	{#if details.image?.url}
+		<img width="200" src={details.image.url} alt={details.name[0].text} />
+	{/if}
 	{@html PrismicDom.RichText.asHtml(details.description)}
 </div>
+
+<style>
+	.person {
+		clear: both;
+		padding: 20px 0;
+	}
+	img {
+		float: left;
+		margin-top: 20px;
+		margin-right: 20px;
+	}
+</style>
